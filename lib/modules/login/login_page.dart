025840c332ec89e46dd9551b9c83c9e84aaa0489
Higor <higorpiresoliveira@gmail.com>
2021-06-22 22:08:40 -0300

    :sparkles: login google

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -83,9 +86,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: SocialLoginButton(onTap: () {
-                      debugPrint('teste');
-                    }),
+                    child: SocialLoginButton(onTap: controller.googleSignIn),
                   ),
                 ],
               ),
