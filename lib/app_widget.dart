e9d3e09ac0a34e5ae6b461bfe13bcc7abb511185
Higor <higorpiresoliveira@gmail.com>
2021-06-22 22:34:29 -0300

    :hammer: authentication

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 
+import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
@@ -11,7 +13,12 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: LoginPage(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -86,7 +86,8 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: SocialLoginButton(onTap: controller.googleSignIn),
+                    child: SocialLoginButton(
+                        onTap: () => controller.googleSignIn(context)),
                   ),
                 ],
               ),
